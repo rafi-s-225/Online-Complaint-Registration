90fdace8f36444b4e25154a1aec63a0ea82b4ac6
V-PavanKalyan <vanga.kalyanpavan@gmail.com>
2026-07-05 09:37:19 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Root Node or main starting point
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
